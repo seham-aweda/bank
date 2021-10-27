f5854b5021629836fc2c21348d2835528d89bae5
seham-aweda <awedaseham@gmail.com>
2021-10-27 11:53:28 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './navbarstyle.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router>
+        <div>
+          <nav >
+            <ul className="list">
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/users">Users</Link>
+              </li>
+              <li>
+                <Link to="/admin">Admin</Link>
+              </li>
+              <li>
+                <Link to="/transfer">Transfer</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/admin">
+              <div>drgrdddd</div>
+            </Route>
+            <Route path="/users">
+              <div>d333dd</div>
+            </Route>
+            <Route path="/transfer">
+              <div>dshhhhhd</div>
+            </Route>
+            <Route path="/">
+              <div>dsadd</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
